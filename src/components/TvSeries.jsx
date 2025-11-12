df11d3630d6f3b5ae0696aeb2b6b4dc7e89529d0
Abdulfatai Abubakri <abdulfataiabubakri01@gmail.com>
2025-11-12 21:13:04 +0100

    fix build error

--- src/components/TvSeries.jsx
+++ src/components/TvSeries.jsx
@@ -1,5 +1,5 @@
 import { useFetch } from "../hooks/useFetch";
-import seriesCard,  { seriesCardData } from "./seriesCard";
+import SeriesCard, { SeriesCardData } from "./SeriesCard";
 
 
     const TvSeries = () => {
@@ -13,13 +13,13 @@ import seriesCard,  { seriesCardData } from "./seriesCard";
         Trending Series
       </h1>
       <div className="flex justify-center flex-wrap md:pl-20 gap-3 pt-30 md:pt-40 rounded-xl ">
-        {data.results?.map((seriesCardData, index) => (
-          <seriesCard
-            key={index}
-            id={seriesCardData.id}
-            img={seriesCardData.poster_path}
-            SeriesName={seriesCardData.original_name}
-            date={seriesCardData.first_air_date}
+        {data.results?.map((SeriesCardData, index) => (
+          <SeriesCard
+            key={SeriesCardData.index}
+            id={SeriesCardData.id}
+            img={SeriesCardData.poster_path}
+            SeriesName={SeriesCardData.original_name}
+            date={SeriesCardData.first_air_date}
           />
         ))}
       </div>
